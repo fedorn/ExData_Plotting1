9cfb3143658ecfee4af7166d7d33e6622ce5dc97
Fedor Nikolaev <fsqcds@gmail.com>
2015-09-07 17:36:53 -0400

    plot2

--- plot2.R
+++ plot2.R
@@ -2,7 +2,8 @@ library(dplyr)
 
 data <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", skip=66636, nrow=2880, stringsAsFactors=FALSE,
                  col.names = colnames(read.table("household_power_consumption.txt", nrow = 1, sep=";", header = TRUE)))
+data <- mutate(data, datetime = as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
 
-png(file = "plot1.png", bg = "transparent")
-hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png(file = "plot2.png", bg = "transparent")
+with(data, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
 dev.off()
